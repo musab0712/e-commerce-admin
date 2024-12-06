42398d36f9e0c02157cef0653ffda9e6fbf3dc85
Musab Hassan <musabhassan1999@gmail.com>
2024-12-06 17:50:25 +0530

    Add shoping API

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-route");
-const adminProductsRouter = require("./routes/admin/products-routes")
+const adminProductsRouter = require("./routes/admin/products-routes");
+
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+//const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 main()
   .then(() => console.log("Mongoose Connected"))
@@ -36,6 +43,13 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/admin/products",adminProductsRouter);
+app.use("/api/admin/products", adminProductsRouter);
+
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+//app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
