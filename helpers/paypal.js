e59f97a08c402a8801e9436ba5b53d5de72bc308
Musab Hassan <musabhassan1999@gmail.com>
2024-12-17 14:57:02 +0530

    add payment integration

--- helpers/paypal.js
+++ helpers/paypal.js
@@ -1,9 +1,11 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "",
-  client_id: "",
-  client_secret: "",
+  mode: "sandbox",
+  client_id:
+    "AZZJ0DoZOWKQGEBrv3jPWJ-uCe8E5_SeIaixf9yUbf_hxPsjo6nKmtjTtjll1WowJZw7ps-ot8K6ps91",
+  client_secret:
+    "ENZdywhzC_UH1zQALg39ZF_1f33t2S1y7i8JgEQzz6gNrLDf6Y82T1a5gCQpKdY7lf89KPSopGchERnz",
 });
 
 module.exports = paypal;
